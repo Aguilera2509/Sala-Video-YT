d4a296667c33cc4f125e6c447dc0d23d318ec81d
Jose Aguilera <Ja846699@gmail.com>
2025-06-13 09:45:58 -0400

    Adding a visibilitychange to handle closing of tab from phone

--- src/_components/_slug/Users_Icon_Conection.tsx
+++ src/_components/_slug/Users_Icon_Conection.tsx
@@ -1,23 +1,20 @@
 "use client"
 
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 import { UsersIcon } from "../icons_slug/users_room";
 
 import useSessionStorage from "@/useCustoms/sessionStorage";
 
-import { onValue, ref, remove } from "firebase/database";
+import { onValue, ref, remove, set } from "firebase/database";
 import { database } from "@/_lib/firebaseApi/firebase_credentials";
+import useMobile from "@/useCustoms/sessionMobile";
 
 export function PlayersInRoom(){
     const [viewers, setViewers] = useState<number>(1);
     const [sessionStorageCode] = useSessionStorage("Code_Cinema_Room");
     const [sessionStorageUsername] = useSessionStorage("Username_Cinema_Room");
-    
-    const UserFirebase = useCallback(() => {
-        const user_db = ref(database, `${sessionStorageCode}/users/${sessionStorageUsername}`);
-        remove(user_db);
-    }, [sessionStorageCode, sessionStorageUsername]);
+    const isMobile = useMobile();
 
     useEffect(() => {
         if(!sessionStorageCode) return;
@@ -30,27 +27,48 @@ export function PlayersInRoom(){
                 };
             });
         };
+
+        const UserFirebase = () => {
+            const user_db = ref(database, `${sessionStorageCode}/users/${sessionStorageUsername}`);
+            remove(user_db);
+        };
         
         gettingViewers();
 
+        const handleVisibilityChange = () => {
+            const currentState = document.visibilityState;
+
+            if (currentState === 'hidden') {
+                const user_db = ref(database, `${sessionStorageCode}/users/${sessionStorageUsername}`);
+                remove(user_db);
+            } else if (currentState === 'visible') {
+                set(ref(database, `${sessionStorageCode}/users/` + sessionStorageUsername), { 
+                    username: sessionStorageUsername
+                });
+                set(ref(database, `rooms/${sessionStorageCode}`), { 
+                    code_video: sessionStorageCode
+                });
+            };
+        };
+
+        if (typeof document !== 'undefined' && isMobile) {
+            document.addEventListener('visibilitychange', handleVisibilityChange);
+        }
+
+        if(window && !isMobile){
+            window.addEventListener("beforeunload", UserFirebase);
+        }
+
         return() => {
             UserFirebase();
+            if(window && !isMobile){
+                window.removeEventListener("beforeunload", UserFirebase);
+            }
+            if (typeof document !== 'undefined' && isMobile) {
+                document.removeEventListener('visibilitychange', handleVisibilityChange);
+            }
         };
-    }, [sessionStorageCode, UserFirebase]);
-
-    useEffect(() => {
-        if(!sessionStorageCode) return;
-
-        function beforeunload(){
-            UserFirebase();
-        };
-
-        window.addEventListener("beforeunload", beforeunload);
-
-        return () => {
-            window.removeEventListener("beforeunload", beforeunload);
-        };
-    }, [sessionStorageCode, UserFirebase]);
+    }, [sessionStorageCode, sessionStorageUsername, isMobile]);
 
     return(
         <div className="col-sm-4">
